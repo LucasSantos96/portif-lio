d88bd4d9d0a15b650976083513be8e10a7b8c429
Lucas Santos <santoslukz@gmail.com>
2026-03-28 18:22:30 -0300

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
-import { Space_Grotesk } from "next/font/google";
+import { Space_Grotesk, Geist } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import type { Metadata } from "next";
 import type { RootLayoutProps } from "@/types";
+import { cn } from "@/lib/utils";
 
-
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const spaceGrotesk = Space_Grotesk({
     variable: "--font-space-grotesk",
@@ -20,7 +21,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: RootLayoutProps) {
     return (
-        <html lang="pt-Br">
+        <html lang="pt-Br" className={cn("font-sans", geist.variable)}>
             <body
                 className={`${spaceGrotesk.variable} antialiased`}
             >
